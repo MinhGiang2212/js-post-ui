9d5e2d53276839248ac4c79bca38669eec96dd0d
MinhGiang2212 <minhgiang1295@gmail.com>
2022-09-11 21:38:38 +0700

    interceptor
    -Add a request interceptor
    -Add a response interceptor

--- js/api/postApi.js
+++ js/api/postApi.js
@@ -2,27 +2,27 @@ import axiosClient from './axiosClient';
 
 const postApi = {
   getAll(params) {
-    const url = 'posts';
+    const url = '/posts';
     return axiosClient.get(url, { params });
   },
 
   getById(id) {
-    const url = `posts/${id}`;
+    const url = `/posts/${id}`;
     return axiosClient.get(url);
   },
 
   add(data) {
-    const url = `posts`;
+    const url = `/posts`;
     return axiosClient.post(url, data);
   },
 
   update(data) {
-    const url = `posts/${data.id}`;
-    return axiosClient.patch(url);
+    const url = `/posts/${data.id}`;
+    return axiosClient.patch(url, data);
   },
 
   remove(id) {
-    const url = `posts/${id}`;
+    const url = `/posts/${id}`;
     return axiosClient.detele(url);
   },
 };
--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-import axiosClient from './api/axiosClient';
 import postApi from './api/postApi';
 
 async function main() {
-  const queryParams = {
-    _page: 1,
-    _limit: 5,
-  };
-  const response = await postApi.getAll(queryParams);
-  console.log(response);
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    };
+    const response = await postApi.getAll(queryParams);
+    console.log(response);
+  } catch (error) {}
+
+  await postApi.update({
+    id: 'lea2aa9l7x3a5tg',
+    title: 'Iure aperiam unde456',
+  });
 }
 main();
-
-console.log('hello from main js');
