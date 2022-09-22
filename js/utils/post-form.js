6ad207b381cdf688157e7f7213e4ad155d72c776
MinhGiang2212 <minhgiang1295@gmail.com>
2022-09-22 14:46:58 +0700

    get form values on submit

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -10,9 +10,33 @@ function setFormValue(form, formValues) {
   setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl);
 }
 
+function getFormValues(form) {
+  if (!form) return;
+
+  const formValues = {};
+
+  const formData = new FormData(form);
+  for (const [key, value] of formData) {
+    formValues[key] = value;
+  }
+
+  return formValues;
+}
+
 export function initPostForm({ formId, defaultValue, onSubmit }) {
   const form = document.getElementById(formId);
   if (!form) return;
 
   setFormValue(form, defaultValue);
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('submit');
+
+    //get form values
+    const formValues = getFormValues(form);
+    console.log(formValues);
+    //validation
+    //if valid trigger submit callback
+    //otherwise, show validation errors
+  });
 }
